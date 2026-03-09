e1389a5b80891438f2df5c01dedb28949958fd0a
YuvrajNayak19 <YuvrajNayak3011@gmail.com>
2026-03-09 22:37:36 +0530

    error

--- Frontend/src/components/services/api.js
+++ Frontend/src/components/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://youtube-twitter-fullstack.onrender.com/api/videos",
+  baseURL: "https://youtube-twitter-fullstack.onrender.com/api/v1",
   withCredentials: true
 });
 
